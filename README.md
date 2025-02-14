f9882e3d3dff49d60f0b569b24ad6b85b4aaf6b8
ClezerSimoes <clezer.simoes@gmail.com>
2025-02-14 14:50:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,61 @@
+# mini sistema ERP
+
+    Sobre o Clezer - miniERP
+
+Este software está sendo desenvolvido com o objetivo de aprender e aprimorar minhas 
+habilidades em Python, Tkinter e SQLite. Trata-se de um gerenciador simples de pedidos 
+de vendas e cadastro de clientes.
+
+Além disso, também pretendo utilizá-lo em um dos programas de extensão da faculdade.
+
+Vale ressaltar que este é o primeiro software que desenvolvo sozinho e, certamente, 
+há maneiras mais eficientes de escrever o código. Estou trabalhando dentro das minhas 
+capacidades atuais, mas planejo continuar aprimorando o projeto.
+
+Sugestões de melhoria são sempre bem-vindas! 
+
+##  Funcionalidades
+
+- Cadastro de clientes
+- Registro de pedidos
+- Listagem e gerenciamento de clientes e pedidos
+- Histórico de pedidos
+
+##  Requisitos
+
+- Python
+
+##  Instalação
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/ClezerSimoes/Clezer-miniERP.git
+   ```
+2. Acesse a pasta do projeto:
+   ```bash
+   cd Clezer-miniERP
+   ```
+3. Execute o projeto:
+   ```bash
+   python main.py
+   ```
+
+##  Como Usar
+
+1. Abra o sistema.
+2. Cadastre um novo cliente.
+3. Registre pedidos para os clientes cadastrados.
+4. Consulte a listagem de clientes e pedidos.
+
+##  Melhorias Futuras
+
+- Adicionar um baco de dados SQLite para armazenar as informações.
+- Geração de relatórios.
+
+##  Contato
+
+Caso tenha dúvidas ou sugestões, sinta-se à vontade para entrar em contato!
+https://github.com/ClezerSimoes
+---
+
+**Observação:** Este README deve ser atualizado conforme o projeto evolui! 
---
+++ main.py
@@ -0,0 +1,89 @@
+## IMPORTS
+
+from tkinter import *
+from tkinter import messagebox
+import os
+import webbrowser
+
+## IMPORT JANELAS
+
+import about 
+import cliente_novo
+import cliente_delete
+import cliente_update
+
+## MAIN APP
+
+main_app = Tk()
+main_app.title ("Clezer - miniERP")
+main_app.geometry ("1080x720")
+main_app.configure(background="#dde")
+
+main_app_Dir=os.path.dirname(__file__)
+
+## FUNÇÕES MENU
+
+def abrir_github():
+    webbrowser.open("https://github.com/ClezerSimoes")
+
+def fun_about():
+    about.janela_about()
+
+def semcomando():
+    print("Teste funcionando")
+
+def newClient():
+    cliente_novo.janela_clienteNovo()
+
+def updateClient():
+    cliente_update.janela_clienteUP()
+
+def deleteClient():
+    cliente_delete.janela_deleteCliente()
+
+## MENU
+
+menu_bar = Menu(main_app)
+
+file_menu = Menu(menu_bar, tearoff=0)
+file_menu.add_command(label="New Flie", command=semcomando)
+file_menu.add_command(label="Open Flie...", command=semcomando)
+file_menu.add_command(label="Backup", command=semcomando)
+file_menu.add_separator()
+file_menu.add_command(label="Language", command=semcomando)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=semcomando)
+menu_bar.add_cascade(label="File", menu=file_menu)
+
+clients_menu = Menu(menu_bar, tearoff=0)
+clients_menu.add_command(label="New Client", command=newClient)
+clients_menu.add_command(label="Client List", command=semcomando)
+clients_menu.add_command(label="Update Client", command=updateClient)
+clients_menu.add_separator()
+clients_menu.add_command(label="Delete Client", command=deleteClient)
+menu_bar.add_cascade(label="Clients", menu=clients_menu)
+
+orders_menu = Menu(menu_bar, tearoff=0)
+orders_menu.add_command(label="New Order", command=semcomando)
+orders_menu.add_command(label="Order list", command=semcomando)
+orders_menu.add_command(label="Update Order", command=semcomando)
+orders_menu.add_separator()
+orders_menu.add_command(label="Delete Order", command=semcomando)
+menu_bar.add_cascade(label="Orders", menu=orders_menu)
+
+help_menu = Menu(menu_bar, tearoff=0)
+help_menu.add_command(label="My Github Page", command=abrir_github)
+help_menu.add_command(label="About the program...", command=fun_about)
+menu_bar.add_cascade(label="Help", menu=help_menu)
+
+
+main_app.config(menu=menu_bar)
+
+## FRAME 
+
+fr_window1= Frame(main_app, borderwidth=1, relief="groove")
+fr_window1.place(width=1080, height=50)
+fr_window1.place(x=((1080/2)-540), y=10)
+
+Label(fr_window1, text= "GERENCIADOR DE CLIENTES E PEDIDOS", font=("Arial", 14, "bold")).pack(expand=True)
+main_app.mainloop()
